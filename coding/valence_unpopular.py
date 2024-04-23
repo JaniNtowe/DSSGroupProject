ff55098afbe310303fc7ee9be26439fcb844b92c
JaniNtowe <84185466+JaniNtowe@users.noreply.github.com>
2024-04-23 13:24:43 -0400

    all graphs made

--- coding/valence_unpopular.py
+++ coding/valence_unpopular.py
@@ -6,12 +6,12 @@ df = pd.read_csv("unpopular_songs.csv", encoding="latin1")
 plt.close("all")
 
 # Organize the data based on a specific column
-organized_data = df.sort_values(by='danceability')
+organized_data = df.sort_values(by='valence')
 
 # Plot the data
 plt.figure(figsize=(10, 6))  # Adjust the figure size as needed
-plt.bar(organized_data['danceability'], organized_data['danceability'])
+plt.bar(organized_data['valence'], organized_data['valence'])
 plt.xlabel('Title')  # Replace 'Column Names' with a suitable label
-plt.ylabel('danceability')  # Replace 'Value' with a suitable label
-plt.title('Danceability graph')
+plt.ylabel('valence')  # Replace 'Value' with a suitable label
+plt.title('Valence graph')
 plt.show()
