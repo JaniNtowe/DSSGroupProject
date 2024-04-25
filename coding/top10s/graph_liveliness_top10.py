0236d2ff729bb63cf025de0ce154fd0e8c48913a
Michaell <michael.a.lopez574@gmail.com>
2024-04-24 22:56:15 -0400

    added graphs

--- coding/top10s/graph_liveliness_top10.py
+++ coding/top10s/graph_liveliness_top10.py
@@ -7,13 +7,13 @@ df = pd.read_csv("top10s.csv", encoding="latin1")
 plt.close("all")
 
 # Organize the data based on a specific column
-organized_data = df.sort_values(by='bpm')
+organized_data = df.sort_values(by='live')
 
 # Plot the data
 plt.figure(figsize=(10, 6))  # Adjust the figure size as needed
-plt.bar(organized_data['title'], organized_data['bpm'])
+plt.bar(organized_data['title'], organized_data['live'])
 plt.xlabel('Title')  # Replace 'Column Names' with a suitable label
-plt.ylabel('bpm')  # Replace 'Value' with a suitable label
-plt.title('Song BPM graph')
+plt.ylabel('liveliness')  # Replace 'Value' with a suitable label
+plt.title('Liveliness Graph')
 plt.xticks([])
 plt.show()
